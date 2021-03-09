e2389f54c3295cdf2ed64c829ff0a36e72e03598
davidecanevazzi <50942807+davidecanevazzi@users.noreply.github.com>
2021-03-09 11:43:32 +0100

    This is the R code of the main pipeline

---
+++ Haplotyping_pipeline.R
@@ -0,0 +1,11 @@
+
+#This is the code for the aplotyping pipeline
+
+reference="GRCh38_latest_genomic.fna.gz"
+
+program="HapCUT2/"
+
+
+#system(paste0("cd ", reference))
+
+system(paste0("cd ", program))
