332cdac6e7bb1afbf132e2619cd5341b67a69ea7
davidecanevazzi <50942807+davidecanevazzi@users.noreply.github.com>
2021-04-29 10:19:06 +0200

    Update fullHaplo_HLA.R

--- fullHaplo_HLA.R
+++ fullHaplo_HLA.R
@@ -1,11 +1,25 @@
 ### Haplotyping pipeline for HLA given minION fastq reads ###
 
+
+#  ------------------------------------ MODULE ACTIVATION -------------------------
+#module load samtools
+#module load minimap2
+#module load flye
+#----------------------------------------------------------------------------------
+
+
+minimap=FALSE
+samtools=FALSE
+pmd=FALSE
+flye=TRUE
+
+
 arg=commandArgs(trailingOnly = TRUE)
 
 FASTQ=arg[1]
 
 prefix=sub("\\..*", "", FASTQ)
-prefix=paste0(prefix,'_')
+
 #you have to discover how to move around 
 
 # Set up input data
@@ -15,7 +29,7 @@ BAM = paste0(prefix,".bam")
 SORTED_BAM = paste0(prefix,".sorted.bam")
 
 if (is.na(arg[2])) {
-  arg[2]="pmd_output"
+  arg[2]="output"
 }
 
 OUTPUT_DIR = arg[2]
@@ -23,14 +37,7 @@ VCF = paste0(prefix,".vcf")
 
 
 # Set the number of CPUs to use
-THREADS="4"
-
-
-minimap=FALSE
-samtools=FALSE
-pmd=FALSE
-flye=TRUE
-
+THREADS="12"
 
 
 paste0('fastq file --> ', FASTQ)
@@ -59,14 +66,14 @@ if(samtools){
 
 if(pmd){
   #from now pepper-margin-deepvariant
-  
+  prefix=paste0(prefix,'_')
   HLA.bam=paste0(prefix,"HLA.bam")
   
   # The pull command creates pepper_deepvariant_r0.4.sif file locally
   
   #system(paste0("singularity pull docker://kishwars/pepper_deepvariant:r0.4"))
   
-  system(paste0("samtools view ",SORTED_BAM," chr6:29940532-31355875 >",HLA.bam)) #select only the HLA genes
+  system(paste0("samtools view -bS",SORTED_BAM," chr6:29940532-31355875 >",HLA.bam)) #select only the HLA genes
   
   system(paste0("echo ---------------- Executing Pepper-Margin-Deepvariant [3/4] ---------------- "))
   
@@ -78,7 +85,7 @@ if(pmd){
               -f ", REFERENCE," \
               -o ", OUTPUT_DIR, " \
               -p ", prefix, " \
-              -t ${", THREADS, "} \
+              -t ${",THREADS,"} \
               --ont"))
 
 }
@@ -86,25 +93,22 @@ if(pmd){
 
 if(flye){  
   HAPLOTAGGED.bam=list.files(paste0(OUTPUT_DIR, "/intermediate_files"), pattern="*MARGIN_PHASED.PEPPER_SNP_MARGIN.haplotagged.bam", full.names=TRUE)[1]
-}
 
-
-  HLA_A.bam=paste0(prefix,"HLA_A.bam")
-  HLA_B.bam=paste0(prefix,"HLA_B.bam")
-  HLA_C.bam=paste0(prefix,"HLA_C.bam")
+  HLA_A.bam=paste0(prefix,"_HLA_A.bam")
+  HLA_B.bam=paste0(prefix,"_HLA_B.bam")
+  HLA_C.bam=paste0(prefix,"_HLA_C.bam")
   
-  
-
   #here I create subset of the haplotagged bam for each gene
-  system(paste0("samtools view ", HAPLOTAGGED.bam," chr6:29941532-29946870 >",HLA_A.bam))
-  system(paste0("samtools view ",HAPLOTAGGED.bam," chr6:31352875-31358179 >",HLA_B.bam))
-  system(paste0("samtools view ",HAPLOTAGGED.bam," chr6:31267749-31273092  >",HLA_C.bam))
+  system(paste0("samtools view -bS ", HAPLOTAGGED.bam," chr6:29941532-29946870 >",HLA_A.bam))
+  system(paste0("samtools view -bS ", HAPLOTAGGED.bam," chr6:31352875-31358179 >",HLA_B.bam))
+  system(paste0("samtools view -bS ", HAPLOTAGGED.bam," chr6:31267749-31273092 >",HLA_C.bam))
+
+  #31353872-31357188 
+  #mica
   
-if(FALSE){  
   
   #then I execute flye for each gene
   system(paste0("echo ---------------- Executing Flye [4/4] ---------------- "))
-  
   
   #split the haplotypes
   system(paste0("bamtools split -in ", HLA_A.bam, " -tag HP"))
@@ -125,13 +129,16 @@ if(FALSE){
   C2=paste0(prefix,'C2.fa')
   
   #convert each haplotype from .bam to .fa
-  system(paste0("samtools bam2fq ", flye_prefixA, "TAG_HP_1.bam | seqtk seq -A > ", A1))
-  system(paste0("samtools bam2fq ", flye_prefixA, "TAG_HP_2.bam | seqtk seq -A > ", A2))
-  system(paste0("samtools bam2fq ", flye_prefixB, "TAG_HP_1.bam | seqtk seq -A > ", B1))
-  system(paste0("samtools bam2fq ", flye_prefixB, "TAG_HP_2.bam | seqtk seq -A > ", B2))
-  system(paste0("samtools bam2fq ", flye_prefixC, "TAG_HP_1.bam | seqtk seq -A > ", C1))
-  system(paste0("samtools bam2fq ", flye_prefixC, "TAG_HP_2.bam | seqtk seq -A > ", C2))
+  system(paste0("samtools bam2fq ", flye_prefixA, ".TAG_HP_1.bam | seqtk seq -A > ", A1))
+  system(paste0("samtools bam2fq ", flye_prefixA, ".TAG_HP_2.bam | seqtk seq -A > ", A2))
+  system(paste0("samtools bam2fq ", flye_prefixB, ".TAG_HP_1.bam | seqtk seq -A > ", B1))
+  system(paste0("samtools bam2fq ", flye_prefixB, ".TAG_HP_2.bam | seqtk seq -A > ", B2))
+  system(paste0("samtools bam2fq ", flye_prefixC, ".TAG_HP_1.bam | seqtk seq -A > ", C1))
+  system(paste0("samtools bam2fq ", flye_prefixC, ".TAG_HP_2.bam | seqtk seq -A > ", C2))
+}
   
+if(FALSE){  
+    
   #out dirs
   oA1=paste0(OUTPUT_DIR,"/flyeA1/")
   oA2=paste0(OUTPUT_DIR,"/flyeA2/")
@@ -141,16 +148,15 @@ if(FALSE){
   oC2=paste0(OUTPUT_DIR,"/flyeC2/")
   
   #execute de-novo assembly with flye
-  
-  system(paste0("flye --nano-raw ", A1, " --out-dir ", oA1))
-  system(paste0("flye --nano-raw ", A2, " --out-dir ", oA2))
-  system(paste0("flye --nano-raw ", B1, " --out-dir ", oB1))
-  system(paste0("flye --nano-raw ", B2, " --out-dir ", oB2))
-  system(paste0("flye --nano-raw ", C1, " --out-dir ", oC1))
-  system(paste0("flye --nano-raw ", C2, " --out-dir ", oC2))
-  
+  system(paste0("flye --nano-raw ", A1, " --out-dir ", oA1, "--threads 4 -m 1000"))
+  system(paste0("flye --nano-raw ", A2, " --out-dir ", oA2, "--threads 4 -m 1000"))
+  system(paste0("flye --nano-raw ", B1, " --out-dir ", oB1, "--threads 4 -m 1000"))
+  system(paste0("flye --nano-raw ", B2, " --out-dir ", oB2, "--threads 4 -m 1000"))
+  system(paste0("flye --nano-raw ", C1, " --out-dir ", oC1, "--threads 4 -m 1000"))
+  system(paste0("flye --nano-raw ", C2, " --out-dir ", oC2, "--threads 4 -m 1000"))
   
 }
 
 system(paste0("echo ---------------- Finished ---------------- "))
+
 
