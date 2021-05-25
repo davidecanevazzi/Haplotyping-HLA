ce565fbb395407e8366833efed9f5fd8ff1d1b53
davidecanevazzi <50942807+davidecanevazzi@users.noreply.github.com>
2021-05-25 12:12:33 +0200

    Update haplosplit.R

--- haplosplit.R
+++ haplosplit.R
@@ -1,19 +1,23 @@
 ### Haplotyping pipeline for HLA given minION fastq reads ###
 
+paste0 <- function(..., collapse = NULL) {
+  paste(..., sep = "", collapse = collapse)
+}
 
 #  ------------------------------------ MODULE ACTIVATION -------------------------
-system(paste0("module load samtools"))
-system(paste0("module load minimap2"))
-system(paste0("module load flye"))
-system(paste0("module load module load PEPPER DEEPVARIANT MARGIN"))
-systme(paste0("module load bamtools"))
+#system(paste0("module load samtools"))
+#system(paste0("module load minimap2"))
+#system(paste0("module load flye"))
+#system(paste0("module load PEPPER DEEPVARIANT MARGIN"))
+#system(paste0("module load bamtools"))
+#system(paste0("module load tabix"))
 #----------------------------------------------------------------------------------
 
 
 minimap=FALSE
 samtools=FALSE
-pmd=TRUE
-flye=FALSE
+pmd=FALSE
+flye=TRUE
 
 
 arg=commandArgs(trailingOnly = TRUE)
@@ -74,8 +78,8 @@ if(samtools){
   system(paste0("samtools index ",SORTED_BAM)) #index the sorted .bam file
   prefix=paste0(prefix,'_')
   HLA.bam=paste0(prefix,"HLA.bam")
-  system(paste0("samtools view -bS",SORTED_BAM," chr6:29940532-33099696 >",HLA.bam)) #select only the HLA genes
-  
+  system(paste0("samtools view -bS",SORTED_BAM," chr6:29940532-33099696 > HLA.bam")) #select only the HLA genes
+  system(paste0("samtools index HLA.bam"))
   system(paste0("echo ---------------- indexing and sorting completed ---------------- "))
   
 }
@@ -90,28 +94,27 @@ if(pmd){
   
   
   system(paste0("echo ---------------- Executing Pepper-Margin-Deepvariant [3/4] ---------------- "))
-
+  
   system(paste0("mkdir output"))
   system(paste0("mkdir output/logs"))
   system(paste0("mkdir output/intermediate_files"))
   system(paste0("export HDF5_USE_FILE_LOCKING='FALSE'"))
-  system(paste0(" singularity exec /apps/PEPPER/0.4/pepper pepper_snp  call_variant -b ", HLA.bam ," -f  ", REFERENCE , " -t 4 -m ../../../../../scratch/production/DAT/apps/PEPPER_MARGIN_DEEPVARIANT/0.4/pepper_models/PEPPER_SNP_R941_ONT_V4.pkl -o output/pepper_snp/ -r chr6 -s Sample -w 4 -bs 64 --ont 2>&1|tee output//logs/1_pepper_snp.log"))
+  system(paste0("singularity exec /apps/PEPPER/0.4/pepper pepper_snp  call_variant -b  HLA.bam -f  ", REFERENCE , " -t 4 -m ../../../../../scratch/production/DAT/apps/PEPPER_MARGIN_DEEPVARIANT/0.4/pepper_models/PEPPER_SNP_R941_ONT_V4.pkl -o output/pepper_snp/ -r chr6 -s Sample -w 4 -bs 64 --ont 2>&1|tee output//logs/1_pepper_snp.log"))
   system(paste0("mv output/pepper_snp/*.vcf output/PEPPER_SNP_OUPUT.vcf")) 
   system(paste0("bgzip output/PEPPER_SNP_OUPUT.vcf")) 
   system(paste0("tabix -p vcf output/PEPPER_SNP_OUPUT.vcf.gz")) 
   system(paste0("rm -rf output/pepper_snp/"))
-  system(paste0("time margin phase", HLA.bam ," ", REFERENCE,  "/output/PEPPER_SNP_OUPUT.vcf.gz ../../../../../apps/MARGIN/2.2.2/params/misc/allParams.ont_haplotag.json -t 4 -r chr6 -V -o output/MARGIN_PHASED.PEPPER_SNP_MARGIN 2>&1 | tee output/logs/2_margin_haplotag.log"))
+  system(paste0("time margin phase HLA.bam ", REFERENCE,  " output/PEPPER_SNP_OUPUT.vcf.gz ../../../../../apps/MARGIN/2.2.2/params/misc/allParams.ont_haplotag.json -t 4 -r chr6 -V -o output/MARGIN_PHASED.PEPPER_SNP_MARGIN 2>&1 | tee output/logs/2_margin_haplotag.log"))
   system(paste0("mv output/*.bam output/MARGIN_PHASED.PEPPER_SNP_MARGIN.haplotagged.bam"))
-  ststem(paste("samtools index -@4 /output/MARGIN_PHASED.PEPPER_SNP_MARGIN.haplotagged.bam"))
+  system(paste("samtools index -@4 output/MARGIN_PHASED.PEPPER_SNP_MARGIN.haplotagged.bam"))
   
   system(paste0("echo ---------------- PMD completed ---------------- "))
   
-  }
+}
 #From here haplotyping with de-novo assembly with flye
 
 if(flye){  
-  HAPLOTAGGED.bam=list.files(paste0( "output/intermediate_files"), pattern="*MARGIN_PHASED.PEPPER_SNP_MARGIN.haplotagged.bam", full.names=TRUE)[1]
-  
+  HAPLOTAGGED.bam="output/MARGIN_PHASED.PEPPER_SNP_MARGIN.haplotagged.bam"
   #class1
   HLA_A.bam=paste0(prefix,"HLA_A.bam")
   HLA_B.bam=paste0(prefix,"HLA_B.bam")
@@ -126,16 +129,16 @@ if(flye){
   #here I create subset of the haplotagged bam for each gene
   
   #class1
-  system(paste0("samtools view -bS", HAPLOTAGGED.bam," chr6:29941532-29946870 >",HLA_A.bam))
-  system(paste0("samtools view -bS", HAPLOTAGGED.bam," chr6:31352875-31358179 >",HLA_B.bam))
-  system(paste0("samtools view -bS", HAPLOTAGGED.bam," chr6:31267749-31273092 >",HLA_C.bam))
+  system(paste0("samtools view -bS ", HAPLOTAGGED.bam," chr6:29941532-29946870 >",HLA_A.bam))
+  system(paste0("samtools view -bS ", HAPLOTAGGED.bam," chr6:31352875-31358179 >",HLA_B.bam))
+  system(paste0("samtools view -bS ", HAPLOTAGGED.bam," chr6:31267749-31273092 >",HLA_C.bam))
   
   #class2
-  system(paste0("samtools view -bS", HAPLOTAGGED.bam," chr6:33064569-33080748 >",HLA_DPA1.bam))
-  system(paste0("samtools view -bS", HAPLOTAGGED.bam," chr6:33075990-33089696 >",HLA_DPB1.bam))
-  system(paste0("samtools view -bS", HAPLOTAGGED.bam," chr6:32637406-32654846 >",HLA_DQA1.bam))
-  system(paste0("samtools view -bS", HAPLOTAGGED.bam," chr6:32659467-32666657 >",HLA_DQB1.bam))
-  system(paste0("samtools view -bS", HAPLOTAGGED.bam," chr6:32578775-32589848 >",HLA_DRB1.bam))
+  system(paste0("samtools view -bS ", HAPLOTAGGED.bam," chr6:33064569-33080748 >",HLA_DPA1.bam))
+  system(paste0("samtools view -bS ", HAPLOTAGGED.bam," chr6:33075990-33089696 >" ,HLA_DPB1.bam))
+  system(paste0("samtools view -bS ", HAPLOTAGGED.bam," chr6:32637406-32654846 >",HLA_DQA1.bam))
+  system(paste0("samtools view -bS ", HAPLOTAGGED.bam," chr6:32659467-32666657 >",HLA_DQB1.bam))
+  system(paste0("samtools view -bS ", HAPLOTAGGED.bam," chr6:32578775-32589848 >",HLA_DRB1.bam))
   
   #then I execute flye for each gene
   system(paste0("echo ---------------- Executing Flye [4/4] ---------------- "))
@@ -180,7 +183,7 @@ if(flye){
   DPA1_1=paste0(prefix,'DPA1_1.fa')
   DPA1_2=paste0(prefix,'DPA1_2.fa')
   DPB1_1=paste0(prefix,'DPB1_1.fa')
-  DPB2_2=paste0(prefix,'DPB1_2.fa')
+  DPB1_2=paste0(prefix,'DPB1_2.fa')
   DQA1_1=paste0(prefix,'DQA1_1.fa')
   DQA1_2=paste0(prefix,'DQA1_2.fa')
   DQB1_1=paste0(prefix,'DQB1_1.fa')
@@ -191,23 +194,23 @@ if(flye){
   #convert each haplotype from .bam to .fa
   
   #class1
-  system(paste0("samtools bam2fq ", flye_prefixA, "TAG_HP_1.bam | seqtk seq -A > ", A1))
-  system(paste0("samtools bam2fq ", flye_prefixA, "TAG_HP_2.bam | seqtk seq -A > ", A2))
-  system(paste0("samtools bam2fq ", flye_prefixB, "TAG_HP_1.bam | seqtk seq -A > ", B1))
-  system(paste0("samtools bam2fq ", flye_prefixB, "TAG_HP_2.bam | seqtk seq -A > ", B2))
-  system(paste0("samtools bam2fq ", flye_prefixC, "TAG_HP_1.bam | seqtk seq -A > ", C1))
-  system(paste0("samtools bam2fq ", flye_prefixC, "TAG_HP_2.bam | seqtk seq -A > ", C2))
+  system(paste0("samtools bam2fq ", flye_prefixA, ".TAG_HP_1.bam | seqtk seq -A > ", A1))
+  system(paste0("samtools bam2fq ", flye_prefixA, ".TAG_HP_2.bam | seqtk seq -A > ", A2))
+  system(paste0("samtools bam2fq ", flye_prefixB, ".TAG_HP_1.bam | seqtk seq -A > ", B1))
+  system(paste0("samtools bam2fq ", flye_prefixB, ".TAG_HP_2.bam | seqtk seq -A > ", B2))
+  system(paste0("samtools bam2fq ", flye_prefixC, ".TAG_HP_1.bam | seqtk seq -A > ", C1))
+  system(paste0("samtools bam2fq ", flye_prefixC, ".TAG_HP_2.bam | seqtk seq -A > ", C2))
   #class2
-  system(paste0("samtools bam2fq ", flye_prefixDPA1_1, "TAG_HP_1.bam | seqtk seq -A > ", DPA1_1))
-  system(paste0("samtools bam2fq ", flye_prefixDPA1_2, "TAG_HP_2.bam | seqtk seq -A > ", DPA1_2))
-  system(paste0("samtools bam2fq ", flye_prefixDPB1_1, "TAG_HP_1.bam | seqtk seq -A > ", DPB1_1))
-  system(paste0("samtools bam2fq ", flye_prefixDPB1_2, "TAG_HP_2.bam | seqtk seq -A > ", DPB1_2))
-  system(paste0("samtools bam2fq ", flye_prefixDQA1_1, "TAG_HP_1.bam | seqtk seq -A > ", DQA1_1))
-  system(paste0("samtools bam2fq ", flye_prefixDQA1_2, "TAG_HP_2.bam | seqtk seq -A > ", DQA1_2))
-  system(paste0("samtools bam2fq ", flye_prefixDQB1_1, "TAG_HP_1.bam | seqtk seq -A > ", DQB1_1))
-  system(paste0("samtools bam2fq ", flye_prefixDQB1_2, "TAG_HP_2.bam | seqtk seq -A > ", DQB1_2))
-  system(paste0("samtools bam2fq ", flye_prefixDRB1_1, "TAG_HP_1.bam | seqtk seq -A > ", DRB1_1))
-  system(paste0("samtools bam2fq ", flye_prefixDRB1_2, "TAG_HP_2.bam | seqtk seq -A > ", DRB1_2))
+  system(paste0("samtools bam2fq ", flye_prefixDPA1, ".TAG_HP_1.bam | seqtk seq -A > ", DPA1_1))
+  system(paste0("samtools bam2fq ", flye_prefixDPA1, ".TAG_HP_2.bam | seqtk seq -A > ", DPA1_2))
+  system(paste0("samtools bam2fq ", flye_prefixDPB1, ".TAG_HP_1.bam | seqtk seq -A > ", DPB1_1))
+  system(paste0("samtools bam2fq ", flye_prefixDPB1, ".TAG_HP_2.bam | seqtk seq -A > ", DPB1_2))
+  system(paste0("samtools bam2fq ", flye_prefixDQA1, ".TAG_HP_1.bam | seqtk seq -A > ", DQA1_1))
+  system(paste0("samtools bam2fq ", flye_prefixDQA1, ".TAG_HP_2.bam | seqtk seq -A > ", DQA1_2))
+  system(paste0("samtools bam2fq ", flye_prefixDQB1, ".TAG_HP_1.bam | seqtk seq -A > ", DQB1_1))
+  system(paste0("samtools bam2fq ", flye_prefixDQB1, ".TAG_HP_2.bam | seqtk seq -A > ", DQB1_2))
+  system(paste0("samtools bam2fq ", flye_prefixDRB1, ".TAG_HP_1.bam | seqtk seq -A > ", DRB1_1))
+  system(paste0("samtools bam2fq ", flye_prefixDRB1, ".TAG_HP_2.bam | seqtk seq -A > ", DRB1_2))
 }
 
 if(TRUE){  
@@ -236,23 +239,25 @@ if(TRUE){
   #execute de-novo assembly with flye
   
   #class1
-  system(paste0("flye --nano-raw ", A1, " --out-dir ", oA1, "--threads 4 -m 1000 -i 2"))
-  system(paste0("flye --nano-raw ", A2, " --out-dir ", oA2, "--threads 4 -m 1000 -i 2"))
-  system(paste0("flye --nano-raw ", B1, " --out-dir ", oB1, "--threads 4 -m 1000 -i 2"))
-  system(paste0("flye --nano-raw ", B2, " --out-dir ", oB2, "--threads 4 -m 1000 -i 2"))
-  system(paste0("flye --nano-raw ", C1, " --out-dir ", oC1, "--threads 4 -m 1000 -i 2"))
-  system(paste0("flye --nano-raw ", C2, " --out-dir ", oC2, "--threads 4 -m 1000 -i 2"))
+  system(paste0("flye --nano-raw ", A1, " --out-dir ", oA1, " --threads 4 -m 1000 -i 2"))
+  system(paste0("flye --nano-raw ", A2, " --out-dir ", oA2, " --threads 4 -m 1000 -i 2"))
+  system(paste0("flye --nano-raw ", B1, " --out-dir ", oB1, " --threads 4 -m 1000 -i 2"))
+  system(paste0("flye --nano-raw ", B2, " --out-dir ", oB2, " --threads 4 -m 1000 -i 2"))
+  system(paste0("flye --nano-raw ", C1, " --out-dir ", oC1, " --threads 4 -m 1000 -i 2"))
+  system(paste0("flye --nano-raw ", C2, " --out-dir ", oC2, " --threads 4 -m 1000 -i 2"))
   #class2
-  system(paste0("flye --nano-raw ", DPA1_1, " --out-dir ", oDPA1_1, "--threads 4 -m 1000 -i 2"))
-  system(paste0("flye --nano-raw ", DPA1_2, " --out-dir ", oDPA1_2, "--threads 4 -m 1000 -i 2"))
-  system(paste0("flye --nano-raw ", DPB1_1, " --out-dir ", oDPB1_1, "--threads 4 -m 1000 -i 2"))
-  system(paste0("flye --nano-raw ", DPB1_2, " --out-dir ", oDPB1_2, "--threads 4 -m 1000 -i 2"))
-  system(paste0("flye --nano-raw ", DQA1_1, " --out-dir ", oDQA1_1, "--threads 4 -m 1000 -i 2"))
-  system(paste0("flye --nano-raw ", DQA1_2, " --out-dir ", oDQA1_2, "--threads 4 -m 1000 -i 2"))
-  system(paste0("flye --nano-raw ", DQB1_1, " --out-dir ", oDQB1_1, "--threads 4 -m 1000 -i 2"))
-  system(paste0("flye --nano-raw ", DQB1_2, " --out-dir ", oDQB1_2, "--threads 4 -m 1000 -i 2"))
-  system(paste0("flye --nano-raw ", DRB1_1, " --out-dir ", oDRB1_1, "--threads 4 -m 1000 -i 2"))
-  system(paste0("flye --nano-raw ", DRB1_2, " --out-dir ", oDRB1_2, "--threads 4 -m 1000 -i 2"))
+  system(paste0("flye --nano-raw ", DPA1_1, " --out-dir ", oDPA1_1, " --threads 4 -m 1000 -i 2"))
+  system(paste0("flye --nano-raw ", DPA1_2, " --out-dir ", oDPA1_2, " --threads 4 -m 1000 -i 2"))
+  system(paste0("flye --nano-raw ", DPB1_1, " --out-dir ", oDPB1_1, " --threads 4 -m 1000 -i 2"))
+  system(paste0("flye --nano-raw ", DPB1_2, " --out-dir ", oDPB1_2, " --threads 4 -m 1000 -i 2"))
+  system(paste0("flye --nano-raw ", DQA1_1, " --out-dir ", oDQA1_1, " --threads 4 -m 1000 -i 2"))
+  system(paste0("flye --nano-raw ", DQA1_2, " --out-dir ", oDQA1_2, " --threads 4 -m 1000 -i 2"))
+  system(paste0("flye --nano-raw ", DQB1_1, " --out-dir ", oDQB1_1, " --threads 4 -m 1000 -i 2"))
+  system(paste0("flye --nano-raw ", DQB1_2, " --out-dir ", oDQB1_2, " --threads 4 -m 1000 -i 2"))
+  system(paste0("flye --nano-raw ", DRB1_1, " --out-dir ", oDRB1_1, " --threads 4 -m 1000 -i 2"))
+  system(paste0("flye --nano-raw ", DRB1_2, " --out-dir ", oDRB1_2, " --threads 4 -m 1000 -i 2"))
 }
 
 system(paste0("echo ---------------- Finished ---------------- "))
+
+
